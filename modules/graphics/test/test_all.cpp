74efc38cfaf193ee06e76e179bb30a068dfb1a15
Kirill Illarionov <KAIllarionov@sberbank.ru>
2021-03-19 01:44:14 +0300

    test workflow

---
+++ modules/graphics/test/test_all.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kira on 18.03.2021.
+//
+
+#include <gtest/gtest.h>
+
+#include "test_penis.hpp"
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/graphics/test/test_penis.hpp
@@ -0,0 +1,11 @@
+//
+// Created by kira on 18.03.2021.
+//
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+TEST(Penis, penis) {
+    ASSERT_STRNE("penis", "vagina");
+}
